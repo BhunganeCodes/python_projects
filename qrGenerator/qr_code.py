0d4d992cb722b1f36cf96be7377821aa5a55eb74
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2025-05-20 15:35:53 +0200

    Full project

---
+++ qrGenerator/qr_code.py
@@ -0,0 +1,22 @@
+# Install libraries needed
+# Create a function that collects text and converts to QR code
+# Save QR code as image
+# Run the function
+
+import qrcode
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+url = input("Enter your URL: ")
+generate_qrcode(url)
